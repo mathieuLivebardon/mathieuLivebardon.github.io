c4a1e2376325cc62e895fb4e09d4a1f866ddbff8
mathieuLivebardon <tsunoll96@gmail.com>
2024-01-31 19:26:15 +0100

    split html in file

---
+++ js/includeHTML.js
@@ -0,0 +1,11 @@
+function includeHTML(file) {
+  const xmlHttp = new XMLHttpRequest();
+  xmlHttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      document.body.innerHTML += this.responseText;
+    }
+  };
+
+  xmlHttp.open("GET", file, true);
+  xmlHttp.send();
+}
