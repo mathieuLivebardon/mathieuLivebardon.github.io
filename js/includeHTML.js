448f188600648506472de64b811bff66aaa9c6ae
mathieuLivebardon <tsunoll96@gmail.com>
2024-02-14 15:43:36 +0100

    fix include html ; add modal content for IMUV

--- js/includeHTML.js
+++ js/includeHTML.js
@@ -1,11 +1,14 @@
-function includeHTML(file) {
-  const xmlHttp = new XMLHttpRequest();
-  xmlHttp.onreadystatechange = function () {
-    if (this.readyState == 4 && this.status == 200) {
-      document.body.innerHTML += this.responseText;
-    }
-  };
+const includeHTML = (file) => {
+  return new Promise((resolve) => {
+    const xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function () {
+      if (this.readyState == 4 && this.status == 200) {
+        document.body.innerHTML += this.responseText;
+      }
+    };
 
-  xmlHttp.open("GET", file, true);
-  xmlHttp.send();
-}
+    xmlHttp.open("GET", file, true);
+    xmlHttp.send();
+    resolve();
+  });
+};
