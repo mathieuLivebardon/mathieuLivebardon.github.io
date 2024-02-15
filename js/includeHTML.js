d55431d30c02c15a178507b94c079e59d3580589
mathieuLivebardon <mathieu.livebardon@universite-lyon.fr>
2024-02-15 11:26:26 +0100

    fix resolve in promise of includeHtml function

--- js/includeHTML.js
+++ js/includeHTML.js
@@ -1,14 +1,17 @@
 const includeHTML = (file) => {
-  return new Promise((resolve) => {
+  return new Promise((resolve,reject) => {
     const xmlHttp = new XMLHttpRequest();
     xmlHttp.onreadystatechange = function () {
       if (this.readyState == 4 && this.status == 200) {
         document.body.innerHTML += this.responseText;
+        resolve();
+      }
+      else{
+        reject();
       }
     };
 
     xmlHttp.open("GET", file, true);
     xmlHttp.send();
-    resolve();
   });
 };
