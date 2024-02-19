511c72f6573b7092652d870f90e9ff4e5fa4b1c4
Mathieu Livebardon <mathieu.livebardon@universite-lyon.fr>
2024-02-19 16:58:56 +0100

    replace xhr by fetch function

--- js/includeHTML.js
+++ js/includeHTML.js
@@ -1,15 +1,15 @@
-const includeHTML = (file) => {
-  return new Promise((resolve,reject) => {
-    const xmlHttp = new XMLHttpRequest();
-    xmlHttp.onreadystatechange =  () => {
-      if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
-        document.body.innerHTML += xmlHttp.responseText;
-        resolve();
-      }
+const includeHTML = (url) => {
+  return fetch(url).then((response) => {
+    if (!response.ok) {
+      throw new Error('Error network');
+    }
+    return response.text();
+  })
+    .then((html) => {
+      document.body.innerHTML += html;
 
-    };
+    }).catch(error => {
+      console.error('Error fetching HTML file:', error);
+    });
 
-    xmlHttp.open("GET", file, true);
-    xmlHttp.send();
-  });
 };
